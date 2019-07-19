e5716d88a3524b5614d5149804bd0caca0c5dcad
shinyashikis <shinyashikis@gmail.com>
2019-07-19 23:58:18 +0900

    add create file and delete file method

--- src/main/java/com/github/shinyashikis/rest/RestUserdictAction.java
+++ src/main/java/com/github/shinyashikis/rest/RestUserdictAction.java
@@ -1,6 +1,8 @@
 package com.github.shinyashikis.rest;
 
 import static org.elasticsearch.rest.RestRequest.Method.GET;
+import static org.elasticsearch.rest.RestRequest.Method.PUT;
+import static org.elasticsearch.rest.RestRequest.Method.DELETE;
 import static org.elasticsearch.rest.RestStatus.OK;
 
 import java.io.IOException;
@@ -16,13 +18,16 @@ import org.elasticsearch.rest.RestController;
 import org.elasticsearch.rest.RestRequest;
 
 public class RestUserdictAction extends BaseRestHandler {
-
+	// TODO
+	private static final String USER_DICT_DIR = "/usr/share/elasticsearch/userdict/";
+	
     public RestUserdictAction(final Settings settings,
             final RestController controller) {
         super(settings);
 
-        controller.registerHandler(GET, "/{index}/_userdict", this);
         controller.registerHandler(GET, "/_userdict", this);
+        controller.registerHandler(PUT, "/{index}/_userdict", this);
+        controller.registerHandler(DELETE, "/{index}/_userdict", this);
     }
 
     @Override
@@ -33,22 +38,34 @@ public class RestUserdictAction extends BaseRestHandler {
     @Override
     protected RestChannelConsumer prepareRequest(final RestRequest request,
             final NodeClient client) throws IOException {
-        final boolean isPretty = request.hasParam("pretty");
-        final String index = request.param("index");
-        return channel -> {
-            final XContentBuilder builder = JsonXContent.contentBuilder();
-            if (isPretty) {
-                builder.prettyPrint().lfAtEnd();
-            }
-            builder.startObject();
-            if (index != null) {
-                builder.field("index", index);
-            }
-            builder.field("description",
-                    "This is a elasticsearch-userdict response: " + new Date().toString());
-            builder.endObject();
-            channel.sendResponse(new BytesRestResponse(OK, builder));
+    	final String index = request.param("index");
+    	
+    	return channel -> {
+	    	final XContentBuilder builder = JsonXContent.contentBuilder();
+	    	builder.startObject();
+	    	builder.field("index", index);
+	    	
+	    	String dictFilePath = USER_DICT_DIR + String.format("userdict_%s_ja.txt", index);
+	    	
+	    	if (request.getHttpRequest().method().name().contentEquals("PUT")) {
+	    		createUserDicionary(dictFilePath);
+	    	} else if (request.getHttpRequest().method().name().contentEquals("DELETE")) {
+	    		deleteUserDicionary(dictFilePath);
+	    	} else {
+				builder.field("description", "This is a elasticsearch-userdict response: " + new Date().toString());
+	    	}
+	    	
+    		builder.endObject();
+    		channel.sendResponse(new BytesRestResponse(OK, builder));
         };
+    }
+
+    private void createUserDicionary(String dictFilePath) throws IOException {
+    	// TODO
+    }
+
+    private void deleteUserDicionary(String dictFilePath) throws IOException {
+    	// TODO
     }
 
 }
